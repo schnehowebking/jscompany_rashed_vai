7cac84599e38066a8f6742245be27550d11a3805
devshamima <edevtook@gmail.com>
2023-12-02 11:22:38 +0600

    .gitignore

--- vendor/pixinvent/frest-html-laravel-jetstream/resources/views/components/secondary-button.blade.php
+++
@@ -1,3 +0,0 @@
-<button {{ $attributes->merge(['type' => 'button', 'class' => 'btn btn-label-secondary text-uppercase']) }}>
-  {{ $slot }}
-</button>
--- vendor/pixinvent/frest-html-laravel-jetstream/stubs/app.js
+++
@@ -1,4 +0,0 @@
-import * as alpine from 'alpinejs/dist/cdn';
-
-
-export { alpine };
